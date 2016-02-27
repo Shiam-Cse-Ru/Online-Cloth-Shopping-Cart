66008b6fc1ee4c2559e56c955fd2616cf9f5408d
Abdullah Al Shiam <shiam546365@gmail.com>
2016-02-27 15:54:20 +0600

    Added files via upload

---
+++ Shopping/Admin/Footer.php
@@ -0,0 +1,3 @@
+<div id="footer">   
+<center>&copy; 2016 Online Cloth Shopping| Design by Abdullah Al Shiam </center>
+</div>
---
+++ Shopping/Category.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Online Cloth Shopping</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style type="text/css">
+<!--
+.style5 {font-size: 85%; font-weight: bold; color: #a980d5; font-family: Verdana, Arial, Helvetica, sans-serif; }
+.style3 {font-weight: bold}
+.style9 {font-size: 95%; font-weight: bold; color: #003300; font-family: Verdana, Arial, Helvetica, sans-serif; }
+-->
+</style>
+<style>
+#myImg {
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s;
+}
+
+#myImg:hover {opacity: 0.7;}
+
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (image) */
+.modal-content {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+}
+
+/* Caption of Modal Image */
+#caption {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+    text-align: center;
+    color: #ccc;
+    padding: 10px 0;
+    height: 150px;
+}
+
+/* Add Animation */
+.modal-content, #caption {    
+    -webkit-animation-name: zoom;
+    -webkit-animation-duration: 0.6s;
+    animation-name: zoom;
+    animation-duration: 0.6s;
+}
+
+@-webkit-keyframes zoom {
+    from {transform:scale(0)} 
+    to {transform:scale(1)}
+}
+
+@keyframes zoom {
+    from {transform:scale(0)} 
+    to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+    position: absolute;
+    top: 15px;
+    right: 35px;
+    color: #f1f1f1;
+    font-size: 40px;
+    font-weight: bold;
+    transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+    color: #bbb;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+    .modal-content {
+        width: 100%;
+    }
+}
+</style>
+</head>
+<body>
+<div id="wrapper">
+  
+  <?php
+  include "Header.php";
+  ?>
+  
+  <div id="content">
+    <h2><span style="color:#003300"> Product Category</span></h2>
+    
+    <table width="100%" border="1" bordercolor="#a980d4" >
+    
+      <?php
+// Establish Connection with Database
+$con = mysql_connect("localhost","root");
+// Select Database
+mysql_select_db("shopping", $con);
+// Specify the query to execute
+$sql = "select * from Category_Master";
+// Execute query
+$result = mysql_query($sql,$con);
+// Loop through each records 
+while($row = mysql_fetch_array($result))
+{
+$Id=$row['CategoryId'];
+$CategoryName=$row['CategoryName'];
+$Description=$row['Description'];
+$Image=$row['Image'];
+?>
+      <tr>
+       <td class="style3"><img id="myImg" src="Products/<?php echo $Image;?>" width="124" height="124" border="5" /></td>
+        <td class="style3"><div align="left" class="style9 style5"><strong><?php echo $CategoryName;?></strong></div></td>
+        <td class="style3"><div align="left" class="style9 style5"><strong><?php echo $Description;?></strong></div></td>
+        <td class="style3"><a href="Products.php?CategoryId=<?php echo $Id;?>">View Products</a></td>
+      <!-- The Modal -->
+<div id="myModal" class="modal">
+  <span class="close">×</span>
+  <img class="modal-content" id="img01">
+  <div id="caption"></div>
+</div>
+      </tr>
+      <?php
+}
+// Retrieve Number of records returned
+$records = mysql_num_rows($result);
+?>
+     
+      <?php
+// Close the connection
+mysql_close($con);
+?>
+    </table>
+    <table width="100%" border="0" cellspacing="3" cellpadding="3">
+      <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><p><img src="img/Jeans.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+        <td><p><img src="img/asd.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+        <td><p><img src="img/images.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+      </tr>
+      <tr>
+        <td height="26" bgcolor="#d1bae8"><div align="center" class="style9">Jeans</div></td>
+        <td bgcolor="#d1bae8"><div align="center" class="style9">Bleasures</div></td>
+        <td bgcolor="#d1bae8"><div align="center" class="style9">T-Shirts</div></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+  </div>
+ <?php
+ include "Right.php";
+ ?>
+  <div style="clear:both;"></div>
+   <?php
+ include "Footer.php";
+ ?>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+</script>
+</body>
+</html>
